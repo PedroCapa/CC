b2165a8291f6ed742e273c748eb3a11a54f11bfb
PedroCapa <pmccapa@hotmail.com>
2019-04-09 11:08:25 +0100

    :camel: O codigo inicial:camel:

---
+++ UDPClient.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.net.*;
+
+
+/*O agenteUDP so envia e recebe pacotes, o transporteUDP faz o resto
+   TRansfereCC processa o pedido de conexao
+   Enviar e receber datagramas e o agenteUDP
+*/
+
+// Para ja isto pode ser usado para o cliente indicar que ficheiro quer receber e se a conexão foi aceite
+class AgenteUDPClient
+{
+   public static void main(String args[]) throws Exception
+   {
+      BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+      DatagramSocket clientSocket = new DatagramSocket();
+      InetAddress IPAddress = InetAddress.getByName("localhost");
+
+      byte[] sendData = new byte[1024];
+      byte[] receiveData = new byte[1024];
+      String sentence = inFromUser.readLine();
+      sendData = sentence.getBytes();
+      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+      clientSocket.send(sendPacket);
+      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+      clientSocket.receive(receivePacket);
+      String modifiedSentence = new String(receivePacket.getData());
+      System.out.println("FROM SERVER:" + modifiedSentence);
+      clientSocket.close();
+   }
+}
